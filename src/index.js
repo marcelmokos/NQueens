7599c938fe1e3476551f03af215bc8b30673049f
Marcel Mokoš <marcel.mokos@gmail.com>
2017-01-08 23:19:00 +0100

    add vscode debug and parameter for start command

--- src/index.js
+++ src/index.js
@@ -15,28 +15,33 @@ require("console.table");
 const getMsFromHrtime = hrtime => `${(((hrtime[0] * 1e9) + hrtime[1]) / 1e6).toFixed(3)}ms`;
 const getElapsedTimeInMs = start => getMsFromHrtime(process.hrtime(start));
 
-const table = [];
+const runNQueenAlgorithms = (n = 10) => {
+  console.log(`running NQueens for n='${n}'`);
+  const table = [];
 
-for (let i = 1; i < 11; i += 1) {
-  const counts = [];
-  const preformance = [];
+  for (let i = 1; i <= n; i += 1) {
+    const counts = [];
+    const preformance = [];
 
-  Object.keys(algorithms).forEach((key) => {
-    const start = process.hrtime();
-    const result = algorithms[key](i);
-    const time = getElapsedTimeInMs(start);
+    Object.keys(algorithms).forEach((key) => {
+      const start = process.hrtime();
+      const result = algorithms[key](i);
+      const time = getElapsedTimeInMs(start);
 
-    preformance.push(time);
-    counts.push(result);
-  });
+      preformance.push(time);
+      counts.push(result);
+    });
 
-  const count = counts.pop();
-  counts.forEach((item, index) => {
-    console.assert(count === item, `not same result of '${Object.keys(algorithms)[index + 1]}' algorithm i='${i}' - count='${count}' - item='${item}'`);
-  });
-  table.push([String(i), count, ...preformance]);
-}
+    const count = counts.pop();
+    counts.forEach((item, index) => {
+      console.assert(count === item, `not same result of '${Object.keys(algorithms)[index + 1]}' algorithm i='${i}' - count='${count}' - item='${item}'`);
+    });
+    table.push([String(i), count, ...preformance]);
+  }
 
-const header = ["Board", "n", ...Object.keys(algorithms)];
-console.table(header, table);
-console.log("*1 - solutions using symmetry");
+  const header = ["Board", "n", ...Object.keys(algorithms)];
+  console.table(header, table);
+  console.log("*1 - solutions using symmetry");
+};
+
+runNQueenAlgorithms(process.argv[2]);
